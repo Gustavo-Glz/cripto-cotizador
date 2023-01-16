bda99ee7a4a08882d5735603d2e118c2bc0247a8
Gustavo-Glz <gustavo.gonzalez.montalvo@gmail.com>
2023-01-16 13:25:44 -0600

    Agregando diseño a resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -4,7 +4,7 @@ const Resultado = ({ resultado }) => {
     <div className="text-white flex items-center gap-12">
       <img className="block w-24" src={`https://cryptocompare.com/${IMAGEURL}`} alt="Imagen Cripto" />
       <div>
-      <p className="text-2xl">El precio es de: <span className="font-bold">{PRICE}</span></p>
+      <p className="text-xl">El precio es de: <span className="font-bold">{PRICE}</span></p>
       <p className="text-lg">Precio mas alto del dia: <span className="font-bold">{HIGHDAY}</span></p>
       <p className="text-lg">Precio mas bajo del dia: <span className="font-bold">{LOWDAY}</span></p>
       <p className="text-lg">Variacion ultimas 24 horas: <span className="font-bold">{CHANGEPCT24HOUR}</span></p>
